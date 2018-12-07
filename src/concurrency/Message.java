40a7e5291ac24e4f328fe41cf8c61260cd25f3cb
swu-incomm <swu@incomm.com>
2018-12-07 14:36:06 -0500

    add some comments

--- src/concurrency/Message.java
+++ src/concurrency/Message.java
@@ -1,5 +1,11 @@
 package concurrency;
 
 public class Message {
-
+    /*
+        Three methods allow threads to communicate about the lock status of a resource.
+        wait()
+        notify()
+        notifyAll()
+        
+    */
 }
