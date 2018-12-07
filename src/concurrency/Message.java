81398d59b875a28a0ec7cdd211c09ec3333bbbd1
swu-incomm <swu@incomm.com>
2018-12-07 16:29:05 -0500

    Add waiter, notifier and syncronize features

--- src/concurrency/Message.java
+++ src/concurrency/Message.java
@@ -3,9 +3,19 @@ package concurrency;
 public class Message {
     /*
         Three methods allow threads to communicate about the lock status of a resource.
-        wait()
-        notify()
-        notifyAll()
-        
+        object.wait() to suspend a thread to wait until some other thread invokes notify() on the same object 
+        object.notify() wakes up only one thread that waiting on the object 
+        object.notifyAll() wakes up all the threads waiting on the object
     */
+	private String msg;
+	public Message(String msg) {
+		this.msg = msg;
+	}
+	
+	public String getMessage() {
+		 return this.msg;
+	}
+	public void setMessage(String msg) {
+		this.msg = msg;
+	}
 }
---
+++ src/concurrency/Waiter.java
@@ -0,0 +1,24 @@
+package concurrency;
+/*
+ * A class that will wait for other threads to invoke notify methods to complete its processing
+ * */
+public class Waiter implements Runnable {
+	private Message msg;
+	public Waiter(Message m) {
+		this.msg = m;
+	}
+	@Override 
+	public void run() {
+		String name = Thread.currentThread().getName();
+		synchronized (msg) {
+			try {
+				System.out.println(name + " waiting to get notified at time: " + System.currentTimeMillis());
+				msg.wait();
+			}catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(name + " waiter thread got notified at time: " + System.currentTimeMillis());
+			System.out.println(name + " processed: "+ msg.getMessage());
+		}
+	}
+}
