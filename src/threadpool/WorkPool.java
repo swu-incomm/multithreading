ac168aa539f909a22e97b9d58dba13721c4ebbc4
swu-incomm <swu@incomm.com>
2018-12-11 15:47:10 -0500

    add some comments and small fix

--- src/threadpool/WorkPool.java
+++ src/threadpool/WorkPool.java
@@ -10,11 +10,16 @@ public class WorkPool {
 	public static void main(String [] args) throws InterruptedException {
 		RejectedExecutionHandlerImpl rejectionHandler = new RejectedExecutionHandlerImpl();
 		ThreadFactory threadFactory = Executors.defaultThreadFactory();
+		/*
+		 * we are keeping initial pool size as 2, maximum pool size to 4 and work queue size as 2. So if there are 4 running
+		 *  tasks and more tasks are submitted, the work queue will hold only 2 of them and the rest of them will be handled
+		 *   by RejectedExecutionHandlerImpl.
+		 *   */
 		ThreadPoolExecutor executorPool = new ThreadPoolExecutor(2, 4, 10, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(2), threadFactory, rejectionHandler);
 		MyMonitoredThread monitor = new MyMonitoredThread(executorPool, 3);
 		Thread monitorThread = new Thread(monitor);
 		monitorThread.start();
-		for(int i=0;i<10;i++) {
+		for(int i=1;i<=10;i++) {
 			executorPool.execute(new WorkerThread("cmd"+i));
 		}
 		
